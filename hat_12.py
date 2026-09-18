875381d1b0589711da438161f47614ab797f2e25
Nandini Amaravadi <nandiniamaravadi@gmail.com>
2026-09-18 10:35:16 +0530

    Add Hat and Student classes with sorting and input functionality

---
+++ hat_12.py
@@ -0,0 +1,10 @@
+import random
+class Hat:
+    houses=["Griffindor", "Hafflepuff", "Ravenclaw", "Slytherin"]
+
+    @classmethod
+    def sort(cls,name):
+        print(name, "is in", random.choice(cls.houses))
+
+
+Hat.sort("harry")
